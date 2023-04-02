298182991fcaa620b36db0c2d955b338ca7a7c02
Hyungseok Oh <93474297+brotherstone97@users.noreply.github.com>
2023-04-02 17:37:21 +0900

    [Silver V] Title: 배열 합치기, Time: 1772 ms, Memory: 356952 KB -BaekjoonHub

--- 백준/Silver/11728. 배열 합치기/배열 합치기.java
+++ 백준/Silver/11728. 배열 합치기/배열 합치기.java
@@ -1,85 +1,70 @@
+
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.StringTokenizer;
 
 //두 배열을 합치고 정렬
 public class Main {
-    private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
     public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int lengthA = Integer.parseInt(st.nextToken());
-        int lengthB = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
 
-        List<Long> a = new ArrayList<>();
-        List<Long> b = new ArrayList<>();
+        List<Integer> aList = new ArrayList<>();
+        List<Integer> bList = new ArrayList<>();
 
-        List<Long> result = new ArrayList<>();
+        List<Integer> result = new ArrayList<>();
+        StringTokenizer A = new StringTokenizer(br.readLine());
+        StringTokenizer B = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            aList.add(Integer.parseInt(A.nextToken()));
+        }
+        for (int i = 0; i < M; i++) {
+            bList.add(Integer.parseInt(B.nextToken()));
+        }
 
-        createString(a, lengthA);
-        createString(b, lengthB);
+        int i = 0;
+        int j = 0;
 
-        //a.forEach(e->System.out.println(e));
-//        a.forEach(System.out::println);
-//        b.forEach(System.out::println);
-
-
-        //피드백: 나는 포인터 하나로 구현했는데 해설은 배열별 포인터 하나씩 총 두개로 함
-
-        int idxA = 0;
-        int idxB = 0;
-
-        while (idxA < lengthA && idxB < lengthB) {
-            Long elementA = a.get(idxA);
-            Long elementB = b.get(idxB);
-            if (elementA <= elementB) {
-                result.add(elementA);
-                idxA++;
+        while (i < N && j < M) {
+            int a = aList.get(i);
+            int b = bList.get(j);
+            if (a <= b) {
+                result.add(a);
+                i++;
                 continue;
             }
-            result.add(elementB);
-            idxB++;
+            result.add(b);
+            j++;
         }
 
-        if (idxA == lengthA) {
-            for (int i = idxB; i < lengthB; i++)
-                result.add(b.get(i));
-        }else{
-            for (int i = idxA; i < lengthA; i++)
-                result.add(a.get(i));
+        if (i == N) {
+            for (int k = j; k < M; k++) {
+                result.add(bList.get(k));
+            }
+        } else {
+            for (int k = i; k < N; k++) {
+                result.add(aList.get(k));
+            }
         }
-
-//        for (int i = 0; i < result.size(); i++) {
-//            System.out.printf("%d ", result.get(i));
-//        }
-
         StringBuilder sb = new StringBuilder();
-        for (Long e : result) {
-            sb.append(e + " ");
+        for(int e : result){
+            sb.append(e+" ");
         }
+
         System.out.println(sb.toString());
+
+//        result.forEach(e -> System.out.printf("%d ", e));
+//        aList.forEach(e-> System.out.println(e));
+//        bList.forEach(e-> System.out.println(e));
+
+
     }
 
-    private static void createString(List<Long> list, int listLength) throws IOException {
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i = 1; i <= listLength; i++) {
-            String s = st.nextToken();
-            list.add(Long.parseLong(s));
-        }
-    }
 }
-
-
-//이미 정렬 되어있기 때문에
-//반복문 돌며 가장 앞에 있는 두 요소 비교
-//그렇게 하면 O(N)으로 끝낼 수 있다.
-
-//        a.addAll(b);
-//        for (Long aLong : b) {
-//            a.add(aLong);
-//        }
-

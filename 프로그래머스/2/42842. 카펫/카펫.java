969f1a7e6b4d83f905c621e8374f5f4950e65a73
Hyungseok Oh <93474297+brotherstone97@users.noreply.github.com>
2023-10-13 20:43:01 +0900

    [level 2] Title: 카펫, Time: 0.05 ms, Memory: 75.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int brown, int yellow) {
+        int[] answer = {};
+        List<int[]> measures = calcMeasure(yellow);
+        return calcWH(brown, yellow, measures);
+    }
+    
+    private int[] calcWH(int brown, int yellow, List<int[]> measures){
+        for(int[] measure: measures){
+            int brownWidth = measure[0]+2;
+            int brownHeight = measure[1]+2;
+            if(brownWidth * brownHeight - yellow == brown){
+                int[] answer = {brownWidth,brownHeight};
+                return answer;
+            }
+        }
+        return null;
+    }
+    
+    private List<int[]> calcMeasure(int yellow){
+        List<int[]> measures = new ArrayList<>();
+        for(int i=1; i<=yellow; i++){
+            if(yellow%i==0){
+                //i(제수)와 몫은 yellow의 약수
+                //i<=몫
+                int quo = yellow/i;
+                int[] result = {quo,i};
+                measures.add(result);
+                if(i>=quo){
+                    return measures;
+                }
+            }
+        }
+        return measures;
+    }
+}

b6bbf19f3932b6c3533fd9639c48618b1fbb476d
Hyungseok Oh <93474297+brotherstone97@users.noreply.github.com>
2023-11-18 02:41:35 +0900

    [Gold V] Title: 최소비용 구하기, Time: 532 ms, Memory: 50996 KB -BaekjoonHub

--- 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
@@ -1,46 +1,28 @@
 import java.util.*;
 import java.io.*;
 
-class Node implements Comparable<Node> {
-    int end;
-    int weight;
-
-    Node(int end, int weight) {
-        this.end = end;
-        this.weight = weight;
-    }
-
-    @Override
-    public int compareTo(Node o) {
-        return weight - o.weight;
-        //양수 -> 현재 객체가 더 큼 -> 현재 객체 뒤로감
-        //음수 -> 매개변수 객체가 더 큼 -> 현재 객체 앞으로감
-    }
-
-}
 
 public class Main {
-    static int N, M;
-    static ArrayList<ArrayList<Node>> adj; //인접리스트
-    static int[] dist; //시작점에서 각 정점으로 가는 최단거리
-    static boolean[] visited;
+    private static List<List<Node>> adj;
+    private static int[] minCost;
+    private static boolean[] visited;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-        N = Integer.parseInt(br.readLine());
-        M = Integer.parseInt(br.readLine());
+        int N = Integer.parseInt(br.readLine());
+        int M = Integer.parseInt(br.readLine());
 
         adj = new ArrayList<>();
-        dist = new int[N + 1];
-        visited = new boolean[N + 1];
-
-        Arrays.fill(dist, Integer.MAX_VALUE);
-
         for (int i = 0; i < N + 1; i++) {
             adj.add(new ArrayList<>());
         }
 
+        minCost = new int[N + 1];
+        Arrays.fill(minCost, Integer.MAX_VALUE);
+
+        visited = new boolean[N + 1];
+
+        StringTokenizer st;
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
             int start = Integer.parseInt(st.nextToken());
@@ -50,32 +32,49 @@ public class Main {
             adj.get(start).add(new Node(end, weight));
         }
         st = new StringTokenizer(br.readLine());
-        int startPos = Integer.parseInt(st.nextToken());
-        int endPos = Integer.parseInt(st.nextToken());
-        System.out.println(dijkstra(startPos, endPos));
+        int departure = Integer.parseInt(st.nextToken());
+        int destination = Integer.parseInt(st.nextToken());
 
+        minCost[departure] = 0;
+
+        dijkstra(departure, destination);
+
+        System.out.println(minCost[destination]);
     }
 
-    public static int dijkstra(int start, int end) {
+    private static void dijkstra(int departure, int destination) {
         PriorityQueue<Node> pq = new PriorityQueue<>();
-        pq.offer(new Node(start, 0));
-        dist[start] = 0;
+        pq.offer(new Node(departure, 0));
 
         while (!pq.isEmpty()) {
-            Node currentNode = pq.poll();
-            int current = currentNode.end;
+            Node polled = pq.poll();
+            int currentNode = polled.end;
 
-            if (!visited[current]) {
-                visited[current] = true;
+            if (!visited[currentNode]) {
+                visited[polled.end] = true;
 
-                for (Node node : adj.get(current)) {
-                    if (!visited[node.end] && dist[node.end] > dist[current] + node.weight) {
-                        dist[node.end] = dist[current] + node.weight;
-                        pq.add(new Node(node.end, dist[node.end]));
+                for (Node node : adj.get(currentNode)) {
+                    if (!visited[node.end] && minCost[node.end] > minCost[currentNode] + node.weight) {
+                        minCost[node.end] = minCost[currentNode] + node.weight;
+                        pq.offer(new Node(node.end, minCost[node.end]));
                     }
                 }
             }
         }
-        return dist[end];
+    }
+
+    static class Node implements Comparable<Node> {
+        int end;
+        int weight;
+
+        Node(int end, int weight) {
+            this.end = end;
+            this.weight = weight;
+        }
+
+        @Override
+        public int compareTo(Node o) {
+            return this.weight - o.weight;
+        }
     }
 }

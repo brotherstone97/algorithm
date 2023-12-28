dac43ad7bb44c93b106b1ed3ee107e3f6c134987
Hyungseok Oh <93474297+brotherstone97@users.noreply.github.com>
2023-12-28 17:11:36 +0900

    [level 2] Title: 시소 짝꿍, Time: 0.45 ms, Memory: 71.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.java
@@ -0,0 +1,35 @@
+import java.util.Arrays;
+import java.util.Map;
+import java.util.HashMap;
+
+class Solution {
+    public long solution(int[] weights) {
+        Arrays.sort(weights);
+        Map<Double, Integer> map = new HashMap<>();
+        long answer = 0;
+        
+        for(int e: weights){
+            double a = e*1.0;
+            double b = (e*1.0)/2;
+            double c = (e*2.0)/3;
+            double d = (e*3.0)/4;
+            
+            if(map.containsKey(a)){
+                answer+=map.get(a);
+            }
+            if(map.containsKey(b)){
+                answer+=map.get(b);
+            }
+            if(map.containsKey(c)){
+                answer+=map.get(c);
+            }
+            if(map.containsKey(d)){
+                answer+=map.get(d);
+            }
+            
+            map.put(e*1.0, map.getOrDefault(e*1.0, 0)+1);
+        }
+        
+        return answer;
+    }
+}

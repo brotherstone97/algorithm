ea15351ca1d12cb86f50bff50ff8bca6278d4a70
Hyungseok Oh <93474297+brotherstone97@users.noreply.github.com>
2024-02-08 15:25:08 +0900

    [level 2] Title: 하노이의 탑, Time: 2.33 ms, Memory: 115 MB -BaekjoonHub

--- 프로그래머스/2/12946. 하노이의 탑/하노이의 탑.java
+++ 프로그래머스/2/12946. 하노이의 탑/하노이의 탑.java
@@ -1,31 +1,25 @@
 import java.util.*;
 
 class Solution {
-    private List<int[]> hist = new ArrayList<>();
+    private final List<int[]> list = new ArrayList<>();
     
     public int[][] solution(int n) {
-        hanoi(n, 1,2,3);
+        hanoi(n, 1,3,2);
 
-        int[][] answer = new int[hist.size()][2];
-        
-        for(int i=0; i<hist.size(); i++){
-            answer[i]=hist.get(i);
-        }
-        
-        return answer;
+        return list.toArray(new int[0][]);
     }
     
-    private void hanoi(int n, int from, int via, int to){
+    private void hanoi(int n, int from, int to, int via){
         if(n==1){
             move(from, to);
             return;
         }
-        hanoi(n-1, from, to, via);
+        hanoi(n-1, from, via, to);
         move(from, to);
-        hanoi(n-1, via, from, to);
+        hanoi(n-1, via, to, from);
     }
     
     private void move(int from, int to){
-        hist.add(new int[]{from, to});
+        list.add(new int[]{from, to});
     }
 }

73cbb4e17fdc41ec05a74cfa4ef0b87477cae040
Hyungseok Oh <93474297+brotherstone97@users.noreply.github.com>
2024-02-28 16:56:21 +0900

    [level 3] Title: 양과 늑대, Time: 0.31 ms, Memory: 74.5 MB -BaekjoonHub

--- 프로그래머스/3/92343. 양과 늑대/README.md
+++ 프로그래머스/3/92343. 양과 늑대/README.md
@@ -1,10 +1,10 @@
 # [level 3] 양과 늑대 - 92343 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/92343#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/92343) 
 
 ### 성능 요약
 
-메모리: 75.9 MB, 시간: 0.20 ms
+메모리: 74.5 MB, 시간: 0.31 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 12월 0일 3:30:52
+2024년 02월 28일 16:56:18
 
 ### 문제 설명
 
--- 프로그래머스/3/92343. 양과 늑대/양과 늑대.java
+++ 프로그래머스/3/92343. 양과 늑대/양과 늑대.java
@@ -1,31 +1,31 @@
+//hard
+
 class Solution {
     private boolean[] visited;
-    private int maxSheeps = 1; 
+    private int maxSheep=1;
     
     public int solution(int[] info, int[][] edges) {
         visited = new boolean[info.length];
         visited[0]=true;
-        
-        dfs(1,0, info, edges);
-        
-        return maxSheeps;
+        dfs(1, 0, info, edges);
+        return maxSheep;
     }
     
     private void dfs(int sheep, int wolf, int[] info, int[][] edges){
         if(sheep<=wolf){
-            return;
+           return;
         }
-        
-        maxSheeps = Math.max(maxSheeps, sheep);
+        maxSheep = Math.max(maxSheep, sheep);
         
         for(int[] edge : edges){
             int parent = edge[0];
             int child = edge[1];
-            if(visited[parent] && !visited[child]){
+            
+            if(visited[parent]&&!visited[child]){
                 visited[child]=true;
                 if(info[child]==0){
                     dfs(sheep+1, wolf, info, edges);
-                } else{
+                }else{
                     dfs(sheep, wolf+1, info, edges);
                 }
                 visited[child]=false;
